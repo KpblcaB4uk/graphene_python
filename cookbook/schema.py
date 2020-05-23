1c307414159310688a6db3c2697e5708ddb6354b
KpblcaB4uk <yakushevaanaaaa@gmail.com>
2020-05-23 22:56:08 +0300

    first meet with schema
    created cookbook/ingredients/schema.py and the parent project-level cookbook/schema.py

---
+++ cookbook/schema.py
@@ -0,0 +1,10 @@
+# parent project-level of cookbook/ingredients/schema.py
+import graphene
+import cookbook.ingredients.schema
+
+
+class Query(cookbook.ingredients.schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries as we begin to add more apps to our project
+    pass
+
+schema = graphene.Schema(query=Query)
